d0ea02c6dc634449d467aace3acfe12e10396b68
klosepa <yusuf_kndl-2002@hotmail.com>
2025-10-07 05:54:32 -0400

    ternary-search

---
+++ searching-algorithms/ternary-search.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int ternarySearch(const vector<int>& arr, int target) {
+    int low = 0, high = arr.size() - 1;
+
+    while(low <= high) {
+        int mid1 = low + (high - low)/3;
+        int mid2 = high - (high - low)/3;
+
+        if(arr[mid1] == target) return mid1;
+        if(arr[mid2] == target) return mid2;
+
+        if(target < arr[mid1]) high = mid1 - 1;
+        else if(target > arr[mid2]) low = mid2 + 1;
+        else {
+            low = mid1 + 1;
+            high = mid2 - 1;
+        }
+    }
+
+    return -1; // Bulunamadı
+}
